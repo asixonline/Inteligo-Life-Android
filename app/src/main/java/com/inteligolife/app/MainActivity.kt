aa9e1542d1c1530a4592b5890771000ec7c07a7e
Fischer Tirado <fishdev@gmail.com>
2019-09-21 21:21:14 -0500

    first commit

---
+++ app/src/main/java/com/inteligolife/app/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.inteligolife.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebSettings
+import kotlinx.android.synthetic.main.activity_main.*
+
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_main)
+
+        val settings: WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+        webView.loadUrl("http://dev.asixonline.com/inteligolife_app/")
+
+    }
+}
