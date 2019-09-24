fafeb140e2a4b8a22cc6352c6f6fb8852232ef7f
Fischer Tirado <fishdev@gmail.com>
2019-09-24 18:33:53 -0500

    fix to open on self window

--- app/src/main/java/com/inteligolife/app/MainActivity.kt
+++ app/src/main/java/com/inteligolife/app/MainActivity.kt
@@ -5,18 +5,21 @@ import android.os.Bundle
 import android.webkit.WebSettings
 import kotlinx.android.synthetic.main.activity_main.*
 
-import android.view.WindowManager
+//import android.view.WindowManager
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        //window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
 
         val settings: WebSettings = webView.settings
         settings.javaScriptEnabled = true
         webView.loadUrl("http://dev.asixonline.com/inteligolife_app/")
+        webView.webViewClient = WebViewClient() //Open on self window
+
 
     }
 }
