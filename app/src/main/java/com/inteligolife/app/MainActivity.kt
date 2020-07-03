ccd7431330d65c7795456fd20e57b4de771d9c83
Fischer Tirado <fishdev@gmail.com>
2020-07-03 16:51:43 -0500

    ajuste webview download pdf

--- app/src/main/java/com/inteligolife/app/MainActivity.kt
+++ app/src/main/java/com/inteligolife/app/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.inteligolife.app
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.webkit.WebSettings
-import kotlinx.android.synthetic.main.activity_main.*
-
 //import android.view.WindowManager
-import android.webkit.WebViewClient
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.webkit.DownloadListener
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 import com.onesignal.OneSignal
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,8 +29,22 @@ class MainActivity : AppCompatActivity() {
         val settings: WebSettings = webView.settings
         settings.javaScriptEnabled = true
         webView.loadUrl("https://www.inteligolife.com/app/")
-        webView.webViewClient = WebViewClient() //Open on self window
-
-
+        //webView.webViewClient = WebViewClient() //Open on self window
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                if (url.endsWith(".pdf")) {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+                    return true
+                }
+                return false
+            }
+        }
+/*
+        webView.setDownloadListener(DownloadListener { url, userAgent, contentDisposition, mimetype, contentLength ->
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
+        })
+*/
     }
 }
